a041f05a4c8a8d3f4c83d952da7d4a5d3d8b10c3
lraquel <lraquelr6@gmail.com>
2023-05-03 01:31:36 +0000

    tabla-intermedia

--- src/models.py
+++ src/models.py
@@ -1,42 +1,46 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+favoritos_personajes= Table(
+    "favoritos_personajes",
+    Base.metadata,
+    Column("user_id", ForeignKey("user.id")),
+    Column("character_id", ForeignKey("character.id")),
+)
+
 class User(Base):
     __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
-    username = Column(String(250), nullable=False)
+    name = Column(String(250))
+    lastname = Column(String(250))
+    username = Column(String(250))
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
     favorito = relationship("Favoritos_Personajes")
 
-class Favoritos_Personajes(Base):
-    __tablename__ = 'favorito_personaje'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    user = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    personaje = Column(Integer, ForeignKey('character.id', primary_key=True))
+# class Favoritos_Personajes(Base):
+#     __tablename__ = 'favorito_personaje'
+#     user_id = Column(Integer, ForeignKey('user.id'))
+#     personaje = Column(Integer, ForeignKey('character.id'))
 
 class Character(Base):
     __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    status = Column(String(250), nullable=False)
-    gender = Column(String(250), nullable=False)
-    species = Column(String(250), nullable=False)
-    origin =  Column(String(250), nullable=False)
+    name = Column(String(250))
+    status = Column(String(250))
+    gender = Column(String(250))
+    species = Column(String(250))
+    origin =  Column(String(250))
     favorito = relationship("Favoritos_Personajes")
 
 
